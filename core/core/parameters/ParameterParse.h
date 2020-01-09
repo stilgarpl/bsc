ff38e0906b9ac602de07fc1b67e1a9722b1f447a
stilgar <stilgar.pl@gmail.com>
2020-01-09 10:46:30 +0100

    parser for containers

--- core/core/parameters/ParameterParse.h
+++ core/core/parameters/ParameterParse.h
@@ -12,31 +12,27 @@
 #include <sstream>
 
 template<typename T, typename _ = void>
-struct is_container : std::false_type {
-};
-
-template<typename... Ts>
-struct is_container_helper {
+struct is_container_not_string : std::false_type {
 };
 
 template<typename T>
-struct is_container<
+struct is_container_not_string<
         T,
-        std::enable_if_t<
-                std::conjunction_v<is_container_helper<
-                        typename T::value_type,
-                        typename T::size_type,
-                        typename T::allocator_type,
-                        typename T::iterator,
-                        typename T::const_iterator,
-                        decltype(std::declval<T>().size()),
-                        decltype(std::declval<T>().begin()),
-                        decltype(std::declval<T>().end()),
-                        decltype(std::declval<T>().cbegin()),
-                        decltype(std::declval<T>().cend())
-                >::value, std::negation_v<std::is_same_v<T, std::string>>
-                >
-        >> : public std::true_type {
+        std::void_t<
+                typename T::value_type,
+                typename T::size_type,
+                typename T::allocator_type,
+                typename T::iterator,
+                typename T::const_iterator,
+                decltype(std::declval<T>().size()),
+                decltype(std::declval<T>().begin()),
+                decltype(std::declval<T>().end()),
+                decltype(std::declval<T>().cbegin()),
+                decltype(std::declval<T>().cend()),
+                std::enable_if_t<!std::is_convertible_v<T, std::string>>
+
+        >
+> : public std::true_type {
 };
 
 class ParameterParseException : public std::invalid_argument {
@@ -70,13 +66,14 @@ inline bool parse<bool>(const char* value, int) {
 }
 
 template<typename ParameterType>
-inline ParameterType parse(const char* value, std::enable_if_t<is_container<ParameterType>::value, int>  = 0) {
+ParameterType parse(const char* value, std::enable_if_t<is_container_not_string<ParameterType>::value, int>  = 0) {
     ParameterType container;
-    std::stringstream s;
+    std::stringstream s(value);
     std::string val;
 
-    while (getline(s, val, ','))
-        container.push_back(parse<typename ParameterType::value_type>(val));
+    while (getline(s, val, ',')) { ;
+        container.push_back(parse<typename ParameterType::value_type>(val.c_str()));
+    }
 
     return container;
 
