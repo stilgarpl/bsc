e7eac81951b4272a793d4e28193d188681f4879c
stilgar <krzysztof.tulidowicz@gmail.com>
2020-10-19 16:13:07 +0200

    Updated Dockerfile, changed cmake so that it should build with docker+conan.

--- cmake/configuration/testing.cmake
+++ cmake/configuration/testing.cmake
@@ -1,7 +1,7 @@
-#testing
+
 option(BSC_ENABLE_TESTS "Enable unit tests" ON)
 if (BSC_ENABLE_TESTS)
     enable_testing()
-    find_package(Catch2 CONFIG REQUIRED)
+    find_package(Catch2 REQUIRED)
     include(Catch)
 endif()
